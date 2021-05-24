3742b8bc5fae6a326f9aa4a72fb716101cb96918
Cleopatra27 <cleopatraudouglas@gmail.com>
2021-05-24 12:17:02 -0700

    refactored client code

--- notion_client/client.py
+++ notion_client/client.py
@@ -1,28 +1,34 @@
-# import requests
-#
-# from constants import Constants
-#
-#
-# def send(request, MY_NOTION_TOKEN, action, page_id):
-#     # check action and add path
-#     if action is 'page-create':
-#         url = Constants.NOTION_BASE_URL + 'pages'
-#         response = requests.post(Constants.NOTION_BASE_URL, json=request,
-#                                  headers={"Authorization": 'Basic ' + MY_NOTION_TOKEN,
-#                                           "Notion-Version": Constants.NOTION_VERSION})
-#     elif action is 'page-retrieve':
-#         url = Constants.NOTION_BASE_URL + 'pages/'+page_id
-#         response = requests.get(Constants.NOTION_BASE_URL, json=request,
-#                                  headers={"Authorization": 'Basic ' + MY_NOTION_TOKEN,
-#                                           "Notion-Version": Constants.NOTION_VERSION})
-#     elif action is 'page-update':
-#         url = Constants.NOTION_BASE_URL + 'pages/' + page_id
-#         response = requests.patch(Constants.NOTION_BASE_URL, json=request,
-#                                 headers={"Authorization": 'Basic ' + MY_NOTION_TOKEN,
-#                                          "Notion-Version": Constants.NOTION_VERSION})
-#     elif action is 'token':
-#         url = Constants.NOTION_BASE_URL + 'oauth/' + page_id
-#         response = requests.patch(Constants.NOTION_BASE_URL, json=request,
-#                                 headers={"Authorization": 'Basic ' + MY_NOTION_TOKEN,
-#                                          "Notion-Version": Constants.NOTION_VERSION})
-#     return response
+import requests
+
+from constants import Constants
+
+
+def send(request, notion_token, action, page_id):
+    # check action and add path
+    if action is 'page-create':
+        return post_send(request, notion_token, Constants.NOTION_BASE_URL + 'pages')
+
+    elif action is 'page-retrieve':
+        return get_send(request, notion_token, Constants.NOTION_BASE_URL + 'pages/'+page_id)
+
+    elif action is 'page-update':
+        return patch_send(request, notion_token, Constants.NOTION_BASE_URL + 'pages/'+page_id)
+
+    elif action is 'token':
+        return post_send(request, notion_token, Constants.NOTION_BASE_URL + 'oauth/token')
+
+
+def post_send(request, notion_token, url):
+    return requests.post(url, json=request,
+                             headers={"Authorization": 'Basic ' + notion_token,
+                                      "Notion-Version": Constants.NOTION_VERSION})
+
+def get_send(request, notion_token, url):
+    return requests.get(url, json=request,
+                                 headers={"Authorization": 'Basic ' + notion_token,
+                                          "Notion-Version": Constants.NOTION_VERSION})
+
+def patch_send(request, notion_token, url):
+    return requests.patch(url, json=request,
+                                headers={"Authorization": 'Basic ' + notion_token,
+                                         "Notion-Version": Constants.NOTION_VERSION})

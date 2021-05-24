54d8670c5bf127c0399117cfccfd1b6fe271c4f4
cleopatra douglas <cleopatraudouglas@gmail.com>
2021-05-24 11:07:29 -0700

    Update Example.py

--- Example.py
+++ Example.py
@@ -2,7 +2,7 @@ from oauth import connect
 
 
 def oauth():
-    x = connect("8074b0cf-ac90-4622-ab2e-5070a8a0872e", "https://webhook.site/85f41df3-bb23-4c59-bb3c-080bd41bdeed",None)
+    x = connect(NOTION_CLIENT_ID, REDIRECT_URL,STATE)
     print(x)
 
 

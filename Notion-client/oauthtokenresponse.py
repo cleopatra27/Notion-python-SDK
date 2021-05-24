4c6c7bc7c0735dabf8586c5db7e41f7f79306278
Cleopatra27 <cleopatraudouglas@gmail.com>
2021-05-24 11:32:34 -0700

    renamed file to notion_client

--- Notion-client/oauthtokenresponse.py
+++
@@ -1,6 +0,0 @@
-class OAuthTokenResponse:
-    def __init__(self, access_token, workspace_name, workspace_icon, bot_id):
-        self.access_token = access_token
-        self.workspace_name = workspace_name
-        self.workspace_icon = workspace_icon
-        self.bot_id = bot_id
